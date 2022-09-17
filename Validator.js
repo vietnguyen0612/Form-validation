1a3401e44dc63692bca66e27e187a61406a52591
Viet Nguyen <vanvietn73@gmail.com>
2022-09-17 21:48:37 +0700

    commit 1

---
+++ Validator.js
@@ -0,0 +1,79 @@
+function Validator(option) {
+
+    //Hàm thực hiện validate
+
+    function Validate(inputElement, rule) {
+        var errorMessage = rule.test(inputElement.value)
+        var errorElement = inputElement.parentElement.querySelector('.form-message')
+            if(errorMessage) {
+                errorElement.innerText = errorMessage
+                inputElement.parentElement.classList.add('invalid')
+            }else {
+                errorElement.innerText = ''
+                inputElement.parentElement.classList.remove('invalid')
+            }
+    }
+
+    //lấy element của form cần validate
+
+    var formElement = document.querySelector(option.form)
+
+    if(formElement) {
+        option.rules.forEach(function(rule) {
+            var inputElement = formElement.querySelector(rule.selector)
+            if(inputElement) {
+                // xử lý trường hợp blur ra ngoài
+                inputElement.onblur = function() {
+                    Validate(inputElement, rule)
+                }
+
+                // xử lý mỗi khi người dùng nhập vào input
+                inputElement.oninput = function() {
+                    var errorElement = inputElement.parentElement.querySelector('.form-message')
+                    errorElement.innerText = ''
+                    inputElement.parentElement.classList.remove('invalid')
+                }
+            }
+        })
+    }
+}
+
+//Định nghĩa rule
+//Nguyên tắc của các rule
+//1.khi có lỗi => trả ra message lỗi
+//2.khi không có lỗi không trả ra cái gì cả
+
+Validator.isRequired = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value.trim() ? undefined : 'Vui lòng nhập trường này'
+        }
+    }
+}
+
+Validator.isEmail = function(selector) {
+    return {
+        selector: selector,
+        test: function(value) {
+            var regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ 
+            return regex.test(value) ? undefined : 'Vui lòng nhập email'
+        }
+    }
+}
+Validator.minLength = function(selector, min) {
+    return {
+        selector: selector,
+        test: function(value) {
+                return value.length >= min ? undefined : `Vui lòng nhập tối thiểu ${min} kí tự`
+        }
+    }
+}
+Validator.isConfirmed = function(selector, getConfirmValue) {
+    return {
+        selector: selector,
+        test: function(value) {
+            return value === getConfirmValue() ? undefined : 'Nhập không chính xác'
+        }
+    }
+}   
